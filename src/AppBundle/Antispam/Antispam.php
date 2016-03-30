afe3bf46b2ced219bd189b9a930c0a2b2de658cc
Sébastien Gilleron <seb.gilleron@hotmail.fr>
2016-03-30 18:35:11 +0200

    orm

--- src/AppBundle/Antispam/Antispam.php
+++ src/AppBundle/Antispam/Antispam.php
@@ -6,12 +6,14 @@ class Antispam
 {
     private $antispam;
 
-    public function __construct($antispamLength)
+    public function __construct($antispamLength, \Swift_Mailer $mailer)
     {
         $this->antispamLength = $antispamLength;
+        $this->mailer         = $mailer;
     }
     public function isSpam($text)
     {
+
         return strlen($text) > $this->antispamLength;
     }
 }
